6a99ea6e8ed7a5c38494f7a85433cdbeb319f3fd
Shyggi <sdobado@up.edu.ph>
2024-04-15 01:40:26 +0800

    initial edits on homepage.blade.php and creation of master.blade.php

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -4,17 +4,8 @@
         <title>TheBook</title>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
-        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.bundle.min.js"></script>
-        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css"></script>
-        <script src="https://kit.fontawesome.com/47cd24d297.js" crossorigin="anonymous"></script>
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">  
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>  
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>  
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>  
         <link rel="stylesheet" href="C:\xampp\htdocs\thebook\public\css\homepage.css">
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5/dist/js/bootstrap.bundle.min.js" type="module"></script>
       </head>
 <body>
 
---
+++ resources/views/master.blade.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>TheBook</title>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>  
+        <link rel="stylesheet" href="C:\xampp\htdocs\thebook\public\css\homepage.css">
+      </head>
+<body>
+
+<!-- Image and text -->
+<nav class="navbar navbar-custom">
+    <a class="navbar-brand">
+      <img src="C:\xampp\htdocs\thebook\public\images\logotype.png" height= 40 class="d-inline-block align-top" alt="">
+    </a>
+
+      <div class="dropdown">  
+          <button type="button" class="dropbtn btn-primary dropdown" data-toggle="dropdown"><i class="fa-solid fa-user navbar-icon"></i></button>  
+          <div class="dropdown-menu dropdown-menu-right">  
+              <a class="dropdown-item" href="#"> View Lists </a>  
+              <a class="dropdown-item" href="#"> History </a>  
+              <a class="dropdown-item" href="#"> Log Out </a>  
+          </div>  
+      </div>  
+    
+</nav>
+</body>
+</html>
