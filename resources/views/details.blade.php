5b9e812d905830a55227995c0c88e13b021bcb93
Shyggi <sdobado@up.edu.ph>
2024-04-15 01:09:55 +0800

    added blade php files to initial page views

---
+++ resources/views/details.blade.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>TheBook</title>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.bundle.min.js"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css"></script>
+        <script src="https://kit.fontawesome.com/47cd24d297.js" crossorigin="anonymous"></script>
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@300;600&family=Source+Sans+Pro:ital,wght@0,300;0,600;0,700;1,300&display=swap" rel="stylesheet">
+        <link rel="stylesheet" href="C:\xampp\htdocs\thebook\public\css\details.css">
+      </head>
+<body>
+
+<!-- Image and text -->
+<nav class="navbar navbar-custom">
+    <a class="navbar-brand" href="C:\Users\fred_\Downloads\TheBook\frontend\css\homepage.html">
+      <img src="C:\xampp\htdocs\thebook\public\images\logotype.png" height= 40 class="d-inline-block align-top" >
+    </a>
+
+    <i class="fa-solid fa-user navbar-icon"></i>
+</nav>
+<br>
+
+<a class="btn back" href="C:\Users\fred_\Downloads\TheBook\frontend\html\homepage.html"><i class="fa-solid fa-arrow-left"></i> BACK TO HOMEPAGE</a>
+<br>
+<div class="FlexContainer show">
+    <img src="https://images.gr-assets.com/books/1447303603l/2767052.jpg" class="cover">
+    <!--Book Information-->
+    <div class="info">
+        <div class="genrelist"> <!--Genres-->
+            <div class="genre"> Science Fiction </div>
+            <div class="genre"> Young Adult </div>
+            <div class="genre"> Fiction </div>
+        </div>
+        <div class="title">The Hunger Games</div>
+        <div class="author">Suzanne Collins</div>
+        <div class="desc">Seconds before the Earth is demolished to make way for a galactic freeway, Arthur Dent is plucked off the planet by his friend Ford Prefect, a researcher for the revised edition of The Hitchhiker's Guide to the Galaxy who, for the last fifteen years, has been posing as an out-of-work actor.Together this dynamic pair begin a journey through space aided by quotes from The Hitchhiker's Guide ("A towel is about the most massively useful thing an interstellar hitchhiker can have") and a galaxy-full of fellow travelers: Zaphod Beeblebroxâ€”the two-headed, three-armed ex-hippie and totally out-to-lunch president of the galaxy; Trillian, Zaphod's girlfriend (formally Tricia McMillan), whom Arthur tried to pick up at a cocktail party once upon a time zone; Marvin, a paranoid, brilliant, and chronically depressed robot; Veet Voojagig, a former graduate student who is obsessed with the disappearance of all the ballpoint pens he bought over the years.</div>
+        <div class="buttons dropdown">
+            <div class="btn border2 dropbtn" type="submit">Add to List</div>
+                <div class="dropdown-content">
+                    <a href="#">Winning will make you famous.</a>
+                    <hr>
+                    <a href="#">User's List</a>
+                    <hr>
+                    <a href="#">My Very Long List Full of Books I Probably Will Never Read</a>
+                </div>
+        </div>
+        <div class="btn border2" type="submit">More Info</div>
+    </div>
+</div>
+
+
+
+<br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+
+</body></html>
---
+++ resources/views/list.blade.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>TheBook</title>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.bundle.min.js"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css"></script>
+        <script src="https://kit.fontawesome.com/47cd24d297.js" crossorigin="anonymous"></script>
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@300;600&family=Source+Sans+Pro:ital,wght@0,300;0,600;0,700;1,300&display=swap" rel="stylesheet">
+        <link rel="stylesheet" href="C:\xampp\htdocs\thebook\public\css\recommendation.css">
+        <link href="C:\xampp\htdocs\project-rsas\public\css\select2.min.css" rel="stylesheet">
+      </head>
+<body>
+
+<!-- Image and text -->
+<nav class="navbar navbar-custom">
+    <a class="navbar-brand" href="C:\Users\fred_\Downloads\TheBook\frontend\html\homepage.html">
+      <img src="C:\xampp\htdocs\thebook\public\images\logotype.png" height= 40 class="d-inline-block align-top" >
+    </a>
+
+    <i class="fa-solid fa-user navbar-icon"></i>
+</nav>
+<br>
+
+<a class="btn back" href="C:\Users\fred_\Downloads\TheBook\frontend\html\homepage.html"><i class="fa-solid fa-arrow-left"></i> BACK TO HOMEPAGE</a>
+
+<div class="containered text-center body">
+    <p class="title">LIST NAME</p>
+</div>
+
+<div class="FlexContainer">
+  <a class="FlexItem" href="C:\Users\fred_\Downloads\TheBook\frontend\html\details.html">
+    <img src="https://images.gr-assets.com/books/1447303603l/2767052.jpg" class="cover"><p class="book"> <span class="match">81% match</span><br> <span class="booktitle">The Hunger Games</span><br>Suzanne Collins</p></a>
+  <a class="FlexItem">
+    <img src="https://images.gr-assets.com/books/1255614970l/2.jpg" class="cover"><p class="book"> <span class="match">81% match</span><br> <span class="booktitle">Harry Potter and the Order of the Phoenix</span><br>J.K. Rowling</p></a>
+  <a class="FlexItem">
+    <img src="https://images.gr-assets.com/books/1361975680l/2657.jpg" class="cover"><p class="book"> <span class="match">81% match</span><br> <span class="booktitle">To Kill a Mockingbird</span><br>Harper Lee</p></a>
+  <a class="FlexItem">
+    <img src="https://images.gr-assets.com/books/1320399351l/1885.jpg" class="cover"><p class="book"> <span class="match">81% match</span><br> <span class="booktitle">Pride and Prejudice</span><br>Jane Austen</p></a>
+  <a class="FlexItem">
+    <img src="https://images.gr-assets.com/books/1361039443l/41865.jpg" class="cover"><p class="book"> <span class="match">81% match</span><br> <span class="booktitle">Twilight</span><br>Stephenie Meyer</p></a>
+</div>
+
+<br><br><br>
+<footer>
+    <hr style=" width:90%; margin:auto;">
+    <p class="footer">Made with <3<br>
+    Shygfred Christian Obado</p>
+</footer>
+
+</body>
+
+</html>
